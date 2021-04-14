7965c1eab86d3a5ebc2f3f357c5fd8306815fe68
Stephan Ringel <ringel@adobe.com>
2021-04-14 14:14:03 +0200

    Update deploy workflow

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,5 +20,5 @@ module.exports = {
     ]
   },
   plugins: [`@adobe/gatsby-theme-aio`],
-  pathPrefix: process.env.PATH_PREFIX || '/animate'
+  pathPrefix: process.env.PATH_PREFIX || '/animate/'
 };
